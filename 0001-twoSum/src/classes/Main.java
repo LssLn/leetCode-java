36051209aaa7abb521ba124dd26d45b1121eb128
LssLn <lssln97@gmail.com>
2022-08-11 18:23:11 +0200

    0001

--- 0001-twoSum/src/classes/Main.java
+++ 0001-twoSum/src/classes/Main.java
@@ -3,8 +3,12 @@ package classes;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Solution solution = new Solution();
 
+		int[] array = {2,7,11,15};
+		int target = 9;
+		int[] index =  solution.twoSum(array, target);
+		System.out.println("target: "+target+ "\nindex: " + index[0] + " " + index[1]);
 	}
 
 }
--- 0001-twoSum/src/classes/Solution.java
+++ 0001-twoSum/src/classes/Solution.java
@@ -1,5 +1,24 @@
 package classes;
 
-public class Solution {
-
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+    	int[] resultIndex= {0,0};
+    	int length = nums.length;
+    	int current=0;
+    	int next=0;
+    	
+        for(int i=0;i<length;i++){
+        	current=nums[i];
+        	// confronting current element with all the others (next), whose must be different (unique elements) from current
+        	for(int j=0;j<length;j++) {
+        		if(i != j) { // cannot confront the element with itself
+        			if(nums[i]+nums[j]==target) {
+        				resultIndex[0]=i;
+        				resultIndex[1]=j;
+        			}
+        		}
+        	}
+        }
+        return resultIndex;
+    }
 }
