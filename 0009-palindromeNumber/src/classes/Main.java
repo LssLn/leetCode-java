f1ad2da53d6816b9b79a33891f871a236c200e1f
LssLn <lssln97@gmail.com>
2022-08-18 12:47:36 +0200

    0009

---
+++ 0009-palindromeNumber/src/classes/Main.java
@@ -0,0 +1,18 @@
+package classes;
+
+public class Main {
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		int number=121;
+		boolean isPalindrome = solution.isPalindrome(number);
+		System.out.println("is "+number+" palindrome?: " +isPalindrome);
+		
+		number=1215;
+		isPalindrome = solution.isPalindrome(number);
+		System.out.println("is "+number+" palindrome?: " +isPalindrome);
+		
+		number=15951;
+		isPalindrome = solution.isPalindrome(number);
+		System.out.println("is "+number+" palindrome?: " +isPalindrome);
+	}
+}
---
+++ 0009-palindromeNumber/src/classes/Solution.java
@@ -0,0 +1,22 @@
+package classes;
+
+public class Solution {
+	public int reverseNumber(int x) {
+		int reverse = 0;  
+	      while(x != 0){  
+	          int remainder = x % 10;  
+	          reverse = reverse * 10 + remainder;  
+	          x = x/10;  
+	      }  
+	      System.out.println("The reverse of the given number is: " + reverse);  
+	      return reverse;
+	}
+	public boolean isPalindrome(int x) {
+		int reverse = reverseNumber(x);
+		if(reverse == x) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+}
