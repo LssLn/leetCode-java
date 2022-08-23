40715dccf71dc19f07c67fc59095dd10cee4e160
LssLn <lssln97@gmail.com>
2022-08-24 00:17:14 +0200

    0014

--- 0014-longestCommonPrefix/src/classes/Main.java
+++ 0014-longestCommonPrefix/src/classes/Main.java
@@ -6,5 +6,14 @@ public class Main {
 
 		String[] s = {"flower","flow","flight"};
 		System.out.println(solution.longestCommonPrefix(s));
+		
+		String[] s2 = {"flower","flow"};
+		System.out.println(solution.longestCommonPrefix(s2));
+		
+		String[] s3 = {"sun","suneater"};
+		System.out.println(solution.longestCommonPrefix(s3));
+		
+		String[] s4 = {"smeargle","cat","smear"};
+		System.out.println(solution.longestCommonPrefix(s4));
 	}
 }
--- 0014-longestCommonPrefix/src/classes/Solution.java
+++ 0014-longestCommonPrefix/src/classes/Solution.java
@@ -1,6 +1,7 @@
 package classes;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Solution {
 	 public String longestCommonPrefix(String[] strs) {
@@ -16,40 +17,51 @@ public class Solution {
 	        }
 	        System.out.println(minimumLength);
 	        
-	        //Get the longest common prefix
-	        
-	        ArrayList<String> ss = new ArrayList<>();
-	        int i=0;
-	        //Getting the substr, with dimension = minimumLength, of every string into the string array
-	        for(String s: strs) {
-	        	ss.add(s.substring(0, minimumLength));
-		        System.out.println(ss.get(i));
-		        i++;
+	        while(true) {
+	        	//Get the longest common prefix
+		        Set<String> subStrs = new HashSet<>();
+		        //Getting the substr, with dimension = minimumLength, of every string into the string array
+		        for(String s: strs) {
+		        	subStrs.add(s.substring(0, minimumLength));
+		        }
+		        
+		        if(subStrs.size()>1) {
+		        	//subStrings are not the same
+			       	minimumLength -= 1;
+		        }else {
+		        	for(String s : strs) {
+		        		//retrieving longest common prefix
+		        		if(subStrs.contains(s.substring(0, minimumLength))) {
+		        			prefix = s.substring(0, minimumLength);
+		        			return prefix;
+		        		}
+		        	}
+		        }
 	        }
 	        
 	        
-	        //while(!found) {
-	        	
-		        //substring() - 1st param cannot be equal or smaller than 2nd param, else it Throws:IndexOutOfBoundsException
-	        	if(minimumLength > 0) {
-	        		//if not all ss elements are equals, prefix is not common
-		        	for(int j=0;j<strs.length;j++) {
-			        	//confronting each element of substrings ss
-			        	for(int k=0;k<strs.length;k++) {
-			        		//with eachothers
-			        		if(ss.get(j).equals(ss.get(k))) {
-			        			found=true;
-			        			prefix = ss.get(j);
-			        		}else {
-			        			found=false;
-			        		}
-			        	}
-			        }
-		        	
-		    //   	minimumLength -= 1;
-		        }
-	        //}
-	  
-	        return prefix;
+//	        int count=0;
+//	        while(!found) {
+//	        	count=0;
+//		        //substring() - 1st param cannot be equal or smaller than 2nd param, else it Throws:IndexOutOfBoundsException
+//	        	if(minimumLength > 0) {
+//	        		//if not all ss elements are equals, prefix is not common
+//		        	for(int j=0;j<strs.length;j++) {
+//			        	//confronting each element of substrings ss
+//			        	for(int k=0;k<strs.length;k++) {
+//			        		//with eachothers
+//			        		if(subStrs.get(j).equals(subStrs.get(k))) {
+//			        			count++;
+//			        		}
+//			        	}
+//		        	}
+//			        if(count==strs.length) {
+//		        			prefix = subStrs.get(0);
+//			        }else {
+//				       	minimumLength -= 1;
+//				       	subStrs.removeAll(subStrs);
+//			        }
+//		        }
+//	        }
 	    }
 }
