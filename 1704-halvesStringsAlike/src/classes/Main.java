14a2165d39da2f3bc1d9277016b7aeae43ffec12
LssLn <lssln97@gmail.com>
2022-08-24 08:54:02 +0200

    1704

--- 1704-halvesStringsAlike/src/classes/Main.java
+++ 1704-halvesStringsAlike/src/classes/Main.java
@@ -3,8 +3,25 @@ package classes;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Solution sol = new Solution();
+		String s;
+		boolean res;
+		
+		s="flower";
+		res=sol.halvesAreAlike(s);
+		System.out.println(res);
+		
+		s="book";
+		res=sol.halvesAreAlike(s);
+		System.out.println(res);
+		
+		s="textbook";
+		res=sol.halvesAreAlike(s);
+		System.out.println(res);
+		
+		s="ssssaass";
+		res=sol.halvesAreAlike(s);
+		System.out.println(res);
 	}
 
 }
--- 1704-halvesStringsAlike/src/classes/Solution.java
+++ 1704-halvesStringsAlike/src/classes/Solution.java
@@ -1,5 +1,53 @@
 package classes;
 
 public class Solution {
+	public boolean containsVowels(String c) {
+		if(c.equals("a") || c.equals("e") || c.equals("i") || c.equals("o") || c.equals("u")) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public boolean halvesAreAlike(String s) {
+        boolean result=false;
+        int sLength = s.length();
+        //input string has even length
+        String s1 = s.substring(0,sLength/2);
+        int nVowelsS1 = 0;
+        String s2 = s.substring(sLength/2,sLength);
+        int nVowelsS2 = 0;
 
+        
+        System.out.println(s+" -> "+s1+" | "+s2);
+        
+        //getting s1 vowels
+        for(int j=0; j<s1.length(); j++) {
+        	String c = "" + s1.charAt(j);
+        	c=c.toLowerCase();
+        	if(containsVowels(c)) {
+        		nVowelsS1++;
+        	}
+        }
+        
+      //getting s2 vowels
+        for(int j=0; j<s2.length(); j++) {
+        	String c = "" + s2.charAt(j);
+        	c=c.toLowerCase();
+        	if(containsVowels(c)) {
+        		nVowelsS2++;
+        	}
+        }
+        
+        System.out.println(s1+" vowels: "+nVowelsS1+", "+s2+" vowels: "+nVowelsS2);
+        
+        if(nVowelsS1 == nVowelsS2) {
+        	result=true;
+        }else {
+        	result=false;
+        }
+
+        
+		return result;
+    }
 }
