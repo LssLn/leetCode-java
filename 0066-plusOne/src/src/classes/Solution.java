b1faf4a68b635ae58fbbf56f1b64cfaa45447fe4
LssLn <lssln97@gmail.com>
2022-09-17 02:15:32 +0200

    66

--- 0066-plusOne/src/src/classes/Solution.java
+++ 0066-plusOne/src/src/classes/Solution.java
@@ -4,6 +4,7 @@ public class Solution {
 	public int[] plusOne(int[] digits) {
 		int[] result = new int[101]; //constraint + 1 for worst case
 		
+		//Converting int[] to int
 		StringBuilder sb = new StringBuilder();
 		for(int d : digits) {
 			System.out.print(d+" ");
