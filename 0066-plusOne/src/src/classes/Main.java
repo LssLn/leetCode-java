809d1eae48e77b8433ca349e4db910871aac2de0
LssLn <lssln97@gmail.com>
2022-09-17 12:18:37 +0200

    66

--- 0066-plusOne/src/src/classes/Main.java
+++ 0066-plusOne/src/src/classes/Main.java
@@ -5,8 +5,39 @@ public class Main {
 	public static void main(String[] args) {
 		Solution sol = new Solution();
 		int[] digits = {1,2,3};
+		int[] result;
 		
-		sol.plusOne(digits);
+		result = sol.plusOne(digits);
+		System.out.print("Result: [ ");
+		for(int r : result) {
+			System.out.print(r+" ");
+		}
+		System.out.println("]\n");
+		
+		digits = new int[]{4,3,2,1};
+		result = sol.plusOne(digits);
+		System.out.print("Result: [ ");
+		for(int r : result) {
+			System.out.print(r+" ");
+		}
+		System.out.println("]\n");
+		
+		digits = new int[]{9};
+		result = sol.plusOne(digits);
+		System.out.print("Result: [ ");
+		for(int r : result) {
+			System.out.print(r+" ");
+		}
+		System.out.println("]\n");
+		
+		digits = new int[]{9,8,7,6,5,4,3,2,1,0};
+		result = sol.plusOne(digits);
+		System.out.print("Result: [ ");
+		for(int r : result) {
+			System.out.print(r+" ");
+		}
+		System.out.println("]\n");
+		
 	}
 
 }
--- 0066-plusOne/src/src/classes/Solution.java
+++ 0066-plusOne/src/src/classes/Solution.java
@@ -1,17 +1,37 @@
 package src.classes;
 
 public class Solution {
-	public int[] plusOne(int[] digits) {
-		int[] result = new int[101]; //constraint + 1 for worst case
-		
-		//Converting int[] to int
+	public int[] plusOne(int[] digits) {		
+		//Converting int[] to string
 		StringBuilder sb = new StringBuilder();
+		System.out.print("Array in input: [ ");
 		for(int d : digits) {
 			System.out.print(d+" ");
 			sb.append(d);
 		}
+		System.out.println("]");
+		
+		//converting string to int
 		int number = Integer.parseInt(sb.toString());
 		System.out.println("intDigits: "+number);
+		
+		//adding plus one
+		number++;
+		String strNumber = String.valueOf(number);
+		
+		
+		//converting int to int[]
+		//getting the length of the int value
+		int length = String.valueOf(number).length();
+		System.out.println("length of the converted value +1 "+number+" : "+length);
+		
+		int[] result = new int[length];
+		
+		for(int i=0;i<length;i++) {
+			result[i]=strNumber.charAt(i)-48; //ASCII conversion
+		}
+		
+
 		return result;
     }
 }
