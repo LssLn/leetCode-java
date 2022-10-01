e62e5a5f0910ad553403d46e60487e0defd72405
LssLn <lssln97@gmail.com>
2022-10-01 16:53:32 +0200

    363

--- 0363-maxSumOfTriangleNoLargerThanK/src/classes/SolutionV2.java
+++ 0363-maxSumOfTriangleNoLargerThanK/src/classes/SolutionV2.java
@@ -17,13 +17,11 @@ public class SolutionV2 {
 	public static String ANSI_CYAN = "\u001B[36m";
 	
     public int maxSumSubmatrix(int[][] matrix, int k) {
-    	//given an MxN matrix		=	M columns	N rows
-    	int sum = 0;
-    	Set<Integer> sumSet = new HashSet<>();
-    	
     	int matrixRows = matrix.length;	// N
     	int matrixColumns = matrix[0].length;	// M
+    	//given an MxN matrix		=	M columns	N rows
     	
+    	//printing matrix 
     	System.out.println("Matrix in input: \nSize = "+matrixRows+" rows   |   "+matrixColumns+" column\n");
     	for(int i=0;i<matrixRows;i++) { // N cycles
     		System.out.print("  ");
@@ -33,21 +31,84 @@ public class SolutionV2 {
     		System.out.println();
     	}
     	
-    	/*	we need to store these data:
-    	 * 		- info about which rectangle we are examining (row, row, col, col)
-    	 * 		- the sum of this rectangle
-    	 *  This can be accomplished through an HashMap<Integer[],Integer>
-    	 */
-    	Map<Integer[],Integer> dataMap = new HashMap<>();
+    	int returnVRows = calcMatrixSumRows(matrix,k,matrixRows,matrixColumns);
+//    	int returnVColumns = calcMatrixSumColumns(matrix,k,matrixRows,matrixColumns);
+//    	System.out.println(" {[( "+returnVRows+" |||  "+returnVColumns+" )]}");
+    	return returnVRows;
+    }
+    
+/*    
+    public int calcMatrixSumColumns(int[][] matrix, int k, int matrixRows, int matrixColumns) {
+    	int sum = -100001;	//k is given, by constraints, in the range -105 <= k <= 105. sum is initialized to always be < k
+    	Set<Integer> sumSet = new HashSet<>();
+
+    	// for a given matrix NxM we will have, for each column, N combinations and N-1 iterations
+    	// analyzing rows
+		int columnStart = 1; //will be incremented after each column is finished (each column = M combinations)
+    	for(int columnCounter = 0; columnCounter < matrixColumns; columnCounter ++) {
+    		//first cycle
+    		int columnCounterFixed = columnCounter + 1; // used with the matrix
+    		System.out.println("\n\n - column number : "+ANSI_RED+columnCounterFixed+ANSI_WHITE);
+
+    		//MULTICOLUMNS
+    		int nIterations = matrixColumns+1-columnStart;
+    		System.out.println(" 	there are "+ANSI_CYAN+nIterations+ANSI_WHITE+" iterations for this column");
+    		System.out.println(" 	- going from column number "+columnCounterFixed+ " to column number "+(columnCounter-1+nIterations));
+    		int iterationSum=0;
+    		for(int icolumn=1;icolumn<=nIterations;icolumn++) { // i = number of columns we are considering for this cycle
+        		System.out.println(ANSI_CYAN+" 		ITERATION "+icolumn+ANSI_WHITE);
+
+    			System.out.println(" 		- column "+columnCounterFixed+", analyzing column: "+(columnCounterFixed-1+icolumn));
+    			//selected row doing the scanning of all the combinations with all the columns
+    			int sumColumn = 0;
+    			//translation for the row
+    			System.out.print(" 			");
+    			for(int rowCounter = 0; rowCounter <matrixRows;rowCounter++) {
+            		int rowCounterFixed = rowCounter + 1; // used with the matrix
+//    				System.out.print(" row "+rowCounterFixed);
+            		if(matrix[rowCounterFixed-1][icolumn-1]<=k) { //swap?
+            			sumSet.add(matrix[icolumn-1][rowCounterFixed-1]);
+            		}
+            		sumColumn+=matrix[icolumn-1][rowCounterFixed-1];
+            		if(sumColumn<=k) {
+            			sumSet.add(sumColumn);
+            		}
+    				System.out.print(" ["+matrix[icolumn-1][rowCounterFixed-1]+"] ");
+    			}
+    			iterationSum += sumColumn;
+    			if(iterationSum > k) {
+        			System.out.println(" column sum ==> "+sumColumn+ANSI_RED+"		iteration sum = "+iterationSum+ANSI_WHITE);
+    			}else {
+    				sumSet.add(iterationSum);
+    				System.out.println(" column sum ==> "+sumColumn+ANSI_GREEN+"		iteration sum = "+iterationSum+ANSI_WHITE);
+    			}
+    		}
+    		columnStart++;
+    	}
+    	//calculating the closer to k
+    	for(Integer n:sumSet) {
+    		if(n>sum && n<=k) {
+    			sum=n;
+    		}
+    	}
+    	return sum;
+    	
+    }
+*/    
+    
+    public int calcMatrixSumRows(int[][] matrix, int k, int matrixRows, int matrixColumns) {
+    	int sum = -100001;	//k is given, by constraints, in the range -105 <= k <= 105. sum is initialized to always be < k
+    	Set<Integer> sumSet = new HashSet<>();    	
+
     	// for a given matrix NxM we will have, for each row, N combinations and N-1 iterations
     	// analyzing rows
+		int rowStart = 1; //will be incremented after each row is finished (each row = N combinations)
     	for(int rowCounter = 0; rowCounter < matrixRows; rowCounter ++) {
     		//first cycle
     		int rowCounterFixed = rowCounter + 1; // used with the matrix
     		System.out.println("\n\n - row number : "+ANSI_RED+rowCounterFixed+ANSI_WHITE);
 
     		//MULTIROWS
-    		int rowStart = 1; //will be incremented after each row is finished (each row = N combinations)
     		int nIterations = matrixRows+1-rowStart;
     		System.out.println(" 	there are "+ANSI_CYAN+nIterations+ANSI_WHITE+" iterations for this row");
     		System.out.println(" 	- going from row number "+rowCounterFixed+ " to row number "+(rowCounter-1+nIterations));
@@ -82,11 +143,13 @@ public class SolutionV2 {
     		}
     		rowStart++;
     	}
+    	//calculating the closer to k
     	for(Integer n:sumSet) {
     		if(n>sum && n<=k) {
     			sum=n;
     		}
     	}
     	return sum;
+    	
     }
 }
